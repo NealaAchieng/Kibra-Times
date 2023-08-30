2fcf83a2ae9e66f703979bc34deb11c0a6f66a10
Neala Achieng <achneala49@gmail.com>
2023-08-30 12:43:57 +0300

    installed cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const dotenv=require("dotenv")
+const cors=require("cors")
 const postRoutes=require("./routes/post")
 const authRoutes=require("./routes/auth")
 const app=express()
@@ -7,6 +8,7 @@ dotenv.config()
 const PORT=process.env.PORT || 5000
 const mongoose=require("mongoose")
 app.use(express.json())
+app.use(cors())
 app.use("/api/auth",authRoutes)
 app.use("/api/post",postRoutes)
 app.listen(PORT,()=>{
